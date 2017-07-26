920eaca45c0fc5de858dda092f16b8aea443fcd7
Madhu Kumar D S <madhudskumar@live.in>
2017-07-27 04:07:29 +0530

    fix:search

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,16 +1,34 @@
 import React, {  Component } from 'react';
 
 export default class SearchBar extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            term: ''
+        };
+
+        this.onSearchChange = this.onSearchChange.bind(this)
+    }
+
     render(){
         return (
             <section id="search">
-                <form className="input-group" action="submit">
-                    <input type="text"/>
+                <form className="input-group">
+                    <input type="text"
+                           className="form-control"
+                           value={this.state.term}
+                           onChange={this.onSearchChange}
+                           placeholder="search city"/>
                     <span className="input-group-btn">
                         <button type="submit" className="btn btn-secondary">Submit</button>
                     </span>
                 </form>
             </section>
         )
+    }
+
+    onSearchChange(event){
+        super.setState({term:event.target.value})
     }
 }
